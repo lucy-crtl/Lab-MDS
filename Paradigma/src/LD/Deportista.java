6c1368e7a0bd755aa9690a4f13f7ca369528df5b
Lucy <lmmosquera@espe.edu.ec>
2021-02-26 19:33:28 -0500

    Version1.0

---
+++ Paradigma/src/LD/Deportista.java
@@ -0,0 +1,14 @@
+package LD;
+
+public class Deportista extends Persona {
+	public Deportista(String nombre) {
+	    super(nombre);
+	    // TODO Auto-generated constructor stub
+	  }
+	  @Override
+	  public int correr() {
+	    // TODO Auto-generated method stub
+	    return 7;
+	  }
+	}
+	
